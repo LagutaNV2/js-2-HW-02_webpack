65a28bcc859d108de830e136bd730b5db901d093
Natali LaGuta <n.v.laguta2023@gmail.com>
2024-10-31 19:14:46 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  devServer: {
+    // static: {
+    //   directory: path.join(__dirname, 'dist'),
+    // },
+    // compress: true,
+    port: 9000,
+  },
+  mode: 'development',
+};
